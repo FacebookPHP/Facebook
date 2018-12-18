4df57fc644c67083f087e2f5e2e5fbad375147c0
TernoisElie <39626190+TernoisElie@users.noreply.github.com>
2018-12-18 12:18:52 +0100

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,7 @@
+<link href="../style.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Oswald|Righteous" rel="stylesheet">
+<strong>Bienvenue sur Starbook</strong><br>
+<p style="text-align: justify;">Dans une lointaine galaxie, très lointaine, vos amis se trouvent. </br> La république a besoin de vous, ralliez votre clan rebelle pour vaincre l'Empire, ou passez du côté obscur, mais faudra pas se plaindre de passer pour un méchant. </br>
+Si vous préfèrez être un méchant j'éspère au moins que vous avez de bonne raisons, comme tomber à moitié dans la lave après s'être fait couper en deux par son meilleur ami. Si c'est pas un truc aussi grave, c'est que c'est surement une crise d'ado un peu tardive. Rien de grave.
+</p>
